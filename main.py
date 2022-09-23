60799641e772f2510d2410d2a02e996793630997
Sebastian Metoyer <smetoyer@microsoft.com>
2022-09-23 12:35:04 -0400

    Requested changes

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ COLOR = {
     "ENDC": "\033[0m",
 }
 
+def print_ok():
+    print(COLOR["GREEN"], "OK", COLOR["ENDC"], end = "")
+    
+def print_fail():
+    print(COLOR["RED"], "Failed", COLOR["ENDC"], end = "")
+
 def main():
     conn_str = input("Input connection string: ")
     print()
@@ -38,33 +44,40 @@ def main():
 
 def platform_check():
     if platform.system() == "Windows":
-        print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- Device operating system is supported (" + platform.system() + ")")
+        print_ok()
+        print("- Device operating system is supported (" + platform.system() + ")")
     elif platform.system() == "Linux":
-        print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- Device operating system is supported (" + platform.system() + ")")
+        print_ok()
+        print("- Device operating system is supported (" + platform.system() + ")")
     elif platform.system() == "Darwin":
         # macOS users may not recognize Darwin, so we display their OS name independently
-        print("OK - Device operating system is supported (macOS)")
-        print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- Device operating system is supported (macOS)")
+        print_ok()
+        print("- Device operating system is supported (macOS)")
     else:
-        print(COLOR["RED"], "Failed", COLOR["ENDC"] + "- Device operating system not supported (" + platform.system() + ")")
+        print_fail()
+        print("- Device operating system not supported (" + platform.system() + ")")
 
 def validate_conn_str_format(conn_str):
     split_string = conn_str.split(";")
     # this function only validates format, so we're only concerned that all the key components of a connection string are present
     if "HostName=" in split_string[0] and "DeviceId=" in split_string[1] and "SharedAccessKey=" in split_string[2]:
-        print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- Device connection string is valid")
+        print_ok()
+        print("- Device connection string is valid")
     else:
-        print(COLOR["RED"], "Failed", COLOR["ENDC"] + "- Device connection string is not valid")
+        print_fail()
+        print("- Device connection string is not valid")
 
 def internet_check():
     try:
         sock = socket.create_connection(("www.google.com", 80))
         if sock is not None:
-            print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- Device can DNS resolve and reach an outside URL")
+            print_ok()
+            print("- Device can DNS resolve and reach an outside URL")
             sock.close
         return True
     except OSError:
-        print(COLOR["RED"], "Failed", COLOR["ENDC"] + "- Device cannot DNS resolve and reach an outside URL")
+        print_fail()
+        print("- Device cannot DNS resolve and reach an outside URL")
         pass
     return False
 
@@ -74,17 +87,17 @@ def time_check():
     rawTime = sock.recv(1024)
     dayAndTime = rawTime.decode("utf-8")
     split_dayTime = dayAndTime.split()
-    split_time = split_dayTime[2].split(":")
+    conc_dayTime = split_dayTime[1] + " " + split_dayTime[2]
+    dt_dayTime = datetime.strptime(conc_dayTime, "%y-%m-%d %H:%M:%S")
+    nist_dayTime = (dt_dayTime.timestamp())
 
     local = datetime.now(timezone.utc)
+    local_dayTime = local.timestamp()
 
-    if split_dayTime[1] == local.strftime("%y-%m-%d"):
-        if split_time[0] == local.strftime("%H") and split_time[1] == local.strftime("%M") and math.isclose(float(split_time[2]), float(local.strftime("%S")), abs_tol = 20):
-            print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- System time is synced properly")
-        else:
-            print(COLOR["RED"], "Failed", COLOR["ENDC"] + "- System time is not synced properly")
+    if math.isclose(nist_dayTime, local_dayTime, abs_tol = 300000):
+        print(COLOR["GREEN"], "OK", COLOR["ENDC"] + "- System time is synced properly")
     else:
-            print(COLOR["RED"], "Failed", COLOR["ENDC"] + "- System time is not synced properly")
+        print(COLOR["RED"], "Failed", COLOR["ENDC"] + "- System time is not synced properly")
 
 
 
